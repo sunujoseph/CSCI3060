acd54c875535b30e3a8c15c237fe502328b1b062
Dark-Ronald <philip.walton@ontariotechu.net>
2020-02-26 18:38:49 -0800

    Changed test_documents directory structure, updated FrontEnd source code to reflect changes, updated gitignore, started batch script for running FrontEnd tests

--- FrontEnd/src/main.cpp
+++ FrontEnd/src/main.cpp
@@ -6,6 +6,7 @@
 #include "FileReader.h"
 #include <direct.h>
 #include <mutex>
+#include <assert.h>
 
 using namespace std;
 
@@ -13,23 +14,25 @@ int main() {
 	//get locations of files
 	char currentPath[FILENAME_MAX];
 	_getcwd(currentPath, sizeof(currentPath));
-	int slashes = 1;
+
+#if(_DEBUG)
+	int slashes = 2;
 	for (int i = FILENAME_MAX; i >= 0; i--) {
 		if (currentPath[i] == '\\') {
 			if (slashes == 0) {
-				currentPath[i + 1] = 's';
-				currentPath[i + 2] = 'r';
-				currentPath[i + 3] = 'c';
-				currentPath[i + 4] = '\\';
-				currentPath[i + 5] = '\0';
+				const char testPath[] = "test_documents\\FrontEnd\\";
+				for (int j = 0; j < sizeof(testPath) / sizeof(char); j++) {
+					currentPath[i + j + 1] = testPath[j];
+				}
 				break;
 			}
 			slashes--;
 		}
 		currentPath[i] = -52;
 	}
+#endif
 	string filePath(currentPath);
-	
+	cout << filePath << endl;
 	transactionFileWriter::setPath(filePath);
 	thread fileReaderThread(FileReader::run, filePath);
 	thread fileWriterThread(transactionFileWriter::run);
---
+++ FrontEndTestScript.bat
@@ -0,0 +1,5 @@
+@echo off
+SETLOCAL
+Dir stdout > test.txt
+cd \FrontEnd\FrontEnd
+FrontEnd.exe
